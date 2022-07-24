30ac85b26e6bf2a12b3a3e06d35886dae4e39f1e
Полина Любарова <os29@yandex.ru>
2022-07-24 16:46:10 +0300

    задание 8 часть 1

--- js/pictures-big.js
+++ js/pictures-big.js
@@ -2,10 +2,10 @@
 
 const modalWindow = document.querySelector('.big-picture');
 
-const renderComments = (picture) => {
+const renderComments = (comments) => {
   const bigFragment = document.createDocumentFragment();
 
-  picture.comments.forEach((comment) => {
+  comments.forEach((comment) => {
     const liElement = document.createElement('li');
     liElement.classList.add('social__comment');
 
@@ -27,21 +27,55 @@ const renderComments = (picture) => {
   return bigFragment;
 };
 
+let commentsCount = null;
+let commentsLenth = null;
+let commentsForPicture = null;
+
+const changeCommentsCount = () => {
+  const socialCommentsCount = modalWindow.querySelector('.social__comment-count');
+  socialCommentsCount.innerHTML = `${commentsCount} из <span class="comments-count">${commentsLenth}</span> комментариев`;
+}
+
 const showBigPicture = (picture) => {
   modalWindow.classList.remove('hidden');
-  modalWindow.querySelector('.social__comment-count').classList.add('hidden');
-  modalWindow.querySelector('.comments-loader').classList.add('hidden');
 
   document.body.classList.add('modal-open');
 
   modalWindow.querySelector('.big-picture__img img').src = picture.url;
   modalWindow.querySelector('.likes-count').textContent = picture.likes;
   modalWindow.querySelector('.comments-count').textContent = picture.comments.length;
+  commentsLenth = picture.comments.length;
+  commentsForPicture = picture.comments;
+  modalWindow.querySelector('.comments-loader').classList.remove('hidden');
+  commentsCount = 5;
   modalWindow.querySelector('.social__caption').textContent = picture.description;
   const comments = modalWindow.querySelector('.social__commentimg-upload__starts');
   comments.innerHTML = null;
-  comments.appendChild(renderComments(picture));
+  comments.appendChild(renderComments(picture.comments.slice(0, commentsCount)));
+
+  if(commentsLenth <= 5) {
+    modalWindow.querySelector('.comments-loader').classList.add('hidden');
+    commentsCount = commentsLenth;
+  }
+  changeCommentsCount();
 };
+
+const commentsLoader = modalWindow.querySelector('.comments-loader');
+
+commentsLoader.addEventListener('click', () => {
+
+  if(commentsLenth > commentsCount + 5) {
+    commentsCount += 5;
+  }
+  else {
+    commentsCount = commentsLenth;
+    modalWindow.querySelector('.comments-loader').classList.add('hidden');
+  }
+  const comments = modalWindow.querySelector('.social__commentimg-upload__starts');
+  comments.innerHTML = null;
+  comments.appendChild(renderComments(commentsForPicture.slice(0, commentsCount)));
+  changeCommentsCount();
+} )
 
 const hideBigPhoto = () => {
   modalWindow.classList.add('hidden');
